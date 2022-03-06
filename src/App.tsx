a84ef831665d5d6fe24e3812b8f9dd7048674017
伊東和成 <itokazunari5375@icloud.com>
2022-03-06 13:08:03 +0900

    first-commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  const num: Number = 1;
+  console.log(num);
+  return (
+    <div className="App">
+      <p>Hello world</p>
+    </div>
+  );
+}
+
+export default App;
